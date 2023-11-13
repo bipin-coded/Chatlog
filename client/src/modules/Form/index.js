4455df867c35d2b6826eae7da0d61eff021bcf67
Bipin N <nakrani0108.uk@gmail.com>
2023-11-13 11:21:38 +0000

    Fixed login issue

--- client/src/modules/Form/index.js
+++ client/src/modules/Form/index.js
@@ -29,7 +29,7 @@ function Form({
         e.preventDefault();
         console.log('data :>>', data);
 
-        const res = await fetch(`http://localhost:8000/api/auth/${isSignInPage ? 'login' : 'register'}`, {
+        const res = await fetch(`${process.env.REACT_APP_API_URL}/api/auth/${isSignInPage ? 'login' : 'register'}`, {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify(data)
